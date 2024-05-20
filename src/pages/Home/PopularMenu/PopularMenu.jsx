2a7d1dc853687392047d9c2852119dc2c3f0ead5
meheraz1100 <meheraz1100@gmail.com>
2024-05-21 01:20:09 +0600

    popular menu of bistro boss

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,21 +1,24 @@
 import SectionsTitle from '../../../Components/SectionsTitle/SectionsTitle';
-
-import React, { useEffect, useState } from 'react';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../Hooks/useMenu';
 
 
 
 const PopularMenu = () => {
+
+    // eslint-disable-next-line
+    const [menu, setMenu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
     
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular')
-            setMenu(popularItems)
-        })
-    }, [])
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const popularItems = data.filter(item => item.category === 'popular')
+    //         setMenu(popularItems)
+    //     })
+    // }, [])
     
     console.log(menu)
 
@@ -27,11 +30,14 @@ const PopularMenu = () => {
             ></SectionsTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                     key={item._id}
                     item={item}
                     ></MenuItem>)
                 }
+            </div>
+            <div className='text-center'>
+            <button className='btn btn-outline border-0 border-b-4 mt-4'>View Full Menu</button>
             </div>
         </section>
     );
